8c02e5546431f458668e62631286036e40d85a45
kupuma-ru21 <tech.kupumaru@gmail.com>
2025-01-28 02:13:01 -0800

    feat: add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,18 @@
+// ref: https://reactrouter.com/start/library/routing
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./About";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById("root") as HTMLElement
 );
 
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
+);
